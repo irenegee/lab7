9d1ba952cfb228842309ee1a20fff99179f50d06
irenegee <irg004@ucsd.edu>
2017-02-23 10:45:55 -0800

    like functionality working

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn"). click(like);
+
+	function like(event){
+	ga('send', 'event', 'like', 'click');
 }
+}
+
